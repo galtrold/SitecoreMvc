07b6ac7099fa90747d7fcc9e1b509731f90775fd
cjh <cjh@pentia.dk>
2016-01-27 19:33:57 +0100

    Basic ViewRendering

---
+++ www/Components/Domain/Sessions/Constants.cs
@@ -0,0 +1,22 @@
+﻿namespace Sessions
+{
+    public struct Constants
+    {
+        public struct Fields
+        {
+            public struct Session
+            {
+                public const string Subject = "{A92A212B-3680-45A5-BBB3-31DB8E6B146E}";
+                public const string Description = "{ADC4B4E4-C3EA-425B-B197-C054E5F51264}";
+                public const string Text = "{FB099740-4BD4-4012-9A99-545266B3DFFB}";
+                public const string Speaker = "{B8B9E5B8-637C-4328-9340-AB5527508F40}";
+                public const string Seats = "{1F09C474-864C-448F-A48B-0BD4F8ED59A7}";
+                public const string SeatsLeft = "{C26BFE8B-30EE-4D7F-9744-CF8B98191457}";
+                public const string Start = "{37F6FAAD-BAC6-44F7-860A-24FD0C2D01B3}";
+                public const string End = "{EEC0CD39-2590-4C17-BD95-61715577ED69}";
+
+            }
+        }
+         
+    }
+}
---
+++ www/Components/Domain/Sessions/Models/SessionModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using Sitecore.Mvc.Presentation;
+
+namespace Sessions.Models
+{
+    public class SessionModel : RenderingModel
+    {
+        public SessionModel()
+        {
+            
+        }
+    }
+}
