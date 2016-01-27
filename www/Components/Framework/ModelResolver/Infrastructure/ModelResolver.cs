f002fec7299505428ea4ce5e81d0b58e2e82953b
cjh <cjh@pentia.dk>
2016-01-27 22:47:34 +0100

    ViewRendering with custom model and ModelResolver

---
+++ www/Components/Framework/ModelResolver/Infrastructure/ModelResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Compilation;
+using Sitecore.Mvc.Pipelines.Response.GetModel;
+using Sitecore.Mvc.Presentation;
+
+namespace ModelResolver.Infrastructure
+{
+    public class ModelResolver : GetModelProcessor
+    {
+        public override void Process(GetModelArgs args)
+        {
+            if (args.Result != null)
+                return;
+
+            if (args.Rendering.RenderingType != "Layout" && args.Rendering.RenderingType != "View" && args.Rendering.RenderingType != "r" && args.Rendering.RenderingType != "d")
+                return;
+
+            var viewRenderer = args.Rendering.Renderer as ViewRenderer;
+            if (viewRenderer == null)
+                return;
+
+            var compiledViewType = BuildManager.GetCompiledType(viewRenderer.ViewPath);
+            var baseType = compiledViewType.BaseType;
+
+            if (baseType == null || !baseType.IsGenericType)
+                return;
+
+            var modelType = baseType.GetGenericArguments()[0];
+
+            if (modelType == typeof(object))
+                return;
+
+            args.Result = Activator.CreateInstance(modelType);
+        }
+    }
+}
