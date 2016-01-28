0c4b105439d3c3e4908b7e53ba9f8ed497fe3ad2
cjh <cjh@pentia.dk>
2016-01-28 19:57:09 +0100

    ControllerRendering with SitecoreController doing form postbacks

---
+++ www/Components/Domain/Sessions/Controllers/SessionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using Sessions.ViewModels;
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+
+namespace Sessions.Controllers
+{
+    public class SessionController : SitecoreController
+    {
+        private string _viewPath = "/Components/Domain/Sessions/Views/Session/SessionView.cshtml";
+
+        [HttpGet]
+        public ActionResult SessionView()
+        {
+            var dataItem = Sitecore.Context.Database.GetItem(RenderingContext.Current.Rendering.DataSource);
+            var viewModel = new SessionViewModel(dataItem ?? PageContext.Current.Item);
+            return View(_viewPath, viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult SessionView(SessionViewModel viewModel)
+        {
+
+            // Store registration 
+               
+            SessionViewModel vm = viewModel;
+            try
+            {
+                var attendeeEmain = viewModel.Signup;
+                var dataItem = Sitecore.Context.Database.GetItem(RenderingContext.Current.Rendering.DataSource);
+                vm = new SessionViewModel(dataItem ?? PageContext.Current.Item) { Signup = attendeeEmain };
+            }
+            catch (Exception)
+            {
+                vm.Signup = "Damnit! Kunne ikke finde min datasource :(";
+                return View(_viewPath, vm);
+            }
+            return View(_viewPath, vm);
+        }
+
+    }
+}
---
+++ www/Components/Domain/Sessions/ViewModels/SessionViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+using PT.Framework.SitecoreExtensions;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace Sessions.ViewModels
+{
+    public class SessionViewModel
+    {
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public HtmlString Text { get; set; }
+
+        public string Signup { get; set; }
+
+        public SessionViewModel()
+        {
+        }
+        public SessionViewModel(Item item)
+        {
+            Subject = item.GetString(new ID(Constants.Fields.Session.Subject));
+            Description = item.GetString(new ID(Constants.Fields.Session.Description));
+            Text = new HtmlString(item.GetString(new ID(Constants.Fields.Session.Text)));
+
+        }
+    }
+}
